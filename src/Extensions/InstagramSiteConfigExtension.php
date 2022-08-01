c0cb312cf6ee0f67647a821af15b0ae6901f1718
Iliain <drcv251@gmail.com>
2022-08-01 15:57:39 +1000

    fontawesome included

--- src/Extensions/InstagramSiteConfigExtension.php
+++ src/Extensions/InstagramSiteConfigExtension.php
@@ -16,20 +16,20 @@ use SilverStripe\View\Requirements;
 class InstagramSiteConfigExtension extends DataExtension
 {
     private static $db = [
-        'InstagramToken'	  => 'Varchar(255)',
+        'InstagramToken'    => 'Varchar(255)',
         'InstagramExpires'	=> 'Datetime'
     ];
-  
+
     public function updateCMSFields(FieldList $fields)
     {
         $fields->removeByName(['InstagramToken', 'InstagramExpires']);
         $confLink = Config::inst()->get('Instagram', 'auth_handler_url');
-      
+
         if ($confLink) {
             $notifier = LiteralField::create('Notifier', '');
 
             $instaStatus = $this->owner->InstagramToken ? 'Connected' : 'Not Connected';
-            if ($this->owner->InstagramToken && (date("Y-m-d H:i:s") > $this->owner->InstagramExpires)) {
+            if ($this->owner->InstagramToken && (date('Y-m-d H:i:s') > $this->owner->InstagramExpires)) {
                 $notifier = LiteralField::create('Notifier', '<p class="message error">Your access to Instagram has expired. Please reconnect your account.</p>');
                 $instaStatus = 'Expired';
             }
@@ -42,12 +42,13 @@ class InstagramSiteConfigExtension extends DataExtension
                 TextareaField::create('InstagramToken', 'Your Access Token')->setReadonly(true),
                 LiteralField::create('InstagramButton', '
                     <div class="insta-default">
-                      <a href="' . $confLink . '/instagram/auth?return=' . Director::absoluteBaseURL() . 'instagram/auth' . '" class="insta-default"><i class="fa fa-instagram"></i> Connect Account</a>
+                        <a href="' . $confLink . '/instagram/auth?return=' . Director::absoluteBaseURL() . 'instagram/auth' . '" class="insta-default"><i class="fa fa-instagram"></i> Connect Account</a>
                     </div>'
                 )
             ]);
             
             Requirements::css('iliain/silverstripe-insta:client/css/instagram.css');
+            Requirements::insertHeadTags('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">');
         }
     }
     
