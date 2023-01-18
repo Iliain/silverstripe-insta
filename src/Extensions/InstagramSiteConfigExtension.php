bf692f535c1a64fb1084b777eeee4198268410a6
Iliain <drcv251@gmail.com>
2023-01-19 09:31:15 +1100

    include facebook data and handler

--- src/Extensions/InstagramSiteConfigExtension.php
+++ src/Extensions/InstagramSiteConfigExtension.php
@@ -17,39 +17,69 @@ class InstagramSiteConfigExtension extends DataExtension
 {
     private static $db = [
         'InstagramToken'    => 'Varchar(255)',
-        'InstagramExpires'	=> 'Datetime'
+        'InstagramExpires'	=> 'Datetime',
+        'FacebookToken'     => 'Varchar(255)',
+        'FacebookExpires'	=> 'Datetime'
     ];
 
     public function updateCMSFields(FieldList $fields)
     {
         $fields->removeByName(['InstagramToken', 'InstagramExpires']);
-        $confLink = Config::inst()->get('Instagram', 'auth_handler_url');
+        $instaConfLink = Config::inst()->get('Instagram', 'auth_handler_url');
+        $fbConfLink = Config::inst()->get('Facebook', 'auth_handler_url');
 
-        if ($confLink) {
-            $notifier = LiteralField::create('Notifier', '');
+        if ($instaConfLink) {
+            $instaNotifier = LiteralField::create('InstaNotifier', '');
 
             $instaStatus = $this->owner->InstagramToken ? 'Connected' : 'Not Connected';
             if ($this->owner->InstagramToken && (date('Y-m-d H:i:s') > $this->owner->InstagramExpires)) {
-                $notifier = LiteralField::create('Notifier', '<p class="message error">Your access to Instagram has expired. Please reconnect your account.</p>');
+                $instaNotifier = LiteralField::create('Notifier', '<p class="message error">Your access to Instagram has expired. Please reconnect your account.</p>');
                 $instaStatus = 'Expired';
             }
 
             $fields->addFieldsToTab('Root.Instagram', [
                 HeaderField::create('InstagramHeader', 'Instagram Account'),
-                $notifier,
+                $instaNotifier,
                 ReadonlyField::create('InstagramStatus', 'Status', $instaStatus),
                 DateField::create('InstagramExpires', 'Access Expires')->setReadonly(true),
                 TextareaField::create('InstagramToken', 'Your Access Token')->setReadonly(true),
                 LiteralField::create('InstagramButton', '
                     <div class="insta-default">
-                        <a href="' . $confLink . '/instagram/auth?return=' . Director::absoluteBaseURL() . 'instagram/auth' . '" class="insta-default"><i class="fab fa-instagram"></i> Connect Account</a>
+                        <a href="' . $instaConfLink . '/instagram/auth?return=' . Director::absoluteBaseURL() . 'auth/instagram' . '" class="insta-default"><i class="fab fa-instagram"></i> Connect Account</a>
+                    </div>
+                    <div class="form-group"></div>'
+                )
+            ]);
+        }
+
+        if ($fbConfLink) {
+            $fbNotifier = LiteralField::create('FbNotifier', '');
+
+            $fbStatus = $this->owner->FacebookToken ? 'Connected' : 'Not Connected';
+            if ($this->owner->FacebookToken && (date('Y-m-d H:i:s') > $this->owner->FacebookExpires)) {
+                $fbNotifier = LiteralField::create('FbNotifier', '<p class="message error">Your access to Facebook has expired. Please reconnect your account.</p>');
+                $fbStatus = 'Expired';
+            }
+
+            //Random number to prevent CSRF
+            $state = rand();
+
+            $fields->addFieldsToTab('Root.Instagram', [
+                HeaderField::create('FacebookHeader', 'Facebook Account'),
+                $fbNotifier,
+                ReadonlyField::create('FacebookStatus', 'Status', $fbStatus),
+                DateField::create('FacebookExpires', 'Access Expires')->setReadonly(true),
+                TextareaField::create('FacebookToken', 'Your Access Token')->setReadonly(true),
+                LiteralField::create('FacebookButton', '
+                    <div class="fb-button">
+                        <a href="' . $fbConfLink . '/facebook/auth?state=' . $state . '&return=' . Director::absoluteBaseURL() . 'auth/facebook' . '"><i class="fab fa-facebook-square"></i> Connect Account</a>
                     </div>'
                 )
             ]);
-            
-            Requirements::css('iliain/silverstripe-insta:client/css/instagram.css');
-            Requirements::insertHeadTags('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">');
         }
+
+        Requirements::css('iliain/silverstripe-insta:client/css/instagram.css');
+        Requirements::insertHeadTags('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">');
     }
     
     public function getInstagramPosts($limit = null)
@@ -76,9 +106,9 @@ class InstagramSiteConfigExtension extends DataExtension
         }
     }
     
-    public function getCachedFeed()
+    public function getCachedFeed($type)
     {
-        $cacheFile = Config::inst()->get('Instagram', 'cache_file') ?? 'SocialFeedCache.txt';
+        $cacheFile = Config::inst()->get($type, 'cache_file') ?? $type . '-cache.txt';
         $path = PUBLIC_PATH . DIRECTORY_SEPARATOR . $cacheFile;
 
         $cache = file_get_contents($path);
